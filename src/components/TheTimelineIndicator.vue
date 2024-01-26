08de498c312cc39bb8b31b3cb02ef8d8e7f6edf1
Olga Bogdanova <bon@incom-sr.ru>
2024-01-26 16:50:26 +0300

    Изменение позиции индикатора временной шкалы

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -1,21 +1,29 @@
 <template>
   <hr
     ref="indicatorRef"
-    class="pointer-events-none absolute z-10 w-full border-b-2 border-red-600"
-    :style="{ top: `${calculateTopOffset()}px` }"
+    class="pointer-events-none absolute z-10 w-full border-b-2 border-red-600 transition-all"
+    :style="{ top: calculateTopOffset }"
   />
 </template>
 <script setup>
-import { ref } from 'vue'
-import { HUNDRED_PERCENT, MINUTES_IN_HOUR, SECONDS_IN_DAY, SECONDS_IN_MINUTE } from '../constans'
+import { computed, ref, watchEffect } from 'vue'
+import {
+  HUNDRED_PERCENT,
+  MILLISECONDS_IN_SECOND,
+  MINUTES_IN_HOUR,
+  SECONDS_IN_DAY,
+  SECONDS_IN_MINUTE
+} from '../constans'
 const indicatorRef = ref()
+const secondsSinceMidhight = ref(calculateSecondsSinceMidnight())
+const calculateTopOffset = computed(
+  () =>
+    (calculateSecondsSinceMidnightInPercentage.value * getTimelineHeight()) / HUNDRED_PERCENT + 'px'
+)
 
-function calculateTopOffset() {
-  return (calculateSecondsSinceMidnightInPercentage() * getTimelineHeight()) / HUNDRED_PERCENT
-}
-function calculateSecondsSinceMidnightInPercentage() {
-  return (HUNDRED_PERCENT * calculateSecondsSinceMidnight()) / SECONDS_IN_DAY
-}
+const calculateSecondsSinceMidnightInPercentage = computed(
+  () => (HUNDRED_PERCENT * secondsSinceMidhight.value) / SECONDS_IN_DAY
+)
 function calculateSecondsSinceMidnight() {
   const now = new Date()
   return (
@@ -28,6 +36,12 @@ function calculateSecondsSinceMidnight() {
 function getTimelineHeight() {
   return indicatorRef.value?.parentNode.getBoundingClientRect().height
 }
-getTimelineHeight()
+
+watchEffect(() => {
+  if (secondsSinceMidhight.value > SECONDS_IN_DAY) {
+    secondsSinceMidhight.value = 0
+  }
+})
+setInterval(() => secondsSinceMidhight.value++, MILLISECONDS_IN_SECOND)
 </script>
 <style lang=""></style>
